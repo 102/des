0dba00b2c39353eb6bec0470bb05b890cd40f91c
Roman Gusev <grdesu@gmail.com>
2016-03-25 09:12:36 +0600

    Implement working encoding-decoding with DES cfb

--- des.py
+++ des.py
@@ -4,7 +4,7 @@ ip1 = [58, 50, 42, 34, 26, 18, 10, 2,
        60, 52, 44, 36, 28, 20, 12, 4,
        62, 54, 46, 38, 30, 22, 14, 6,
        64, 56, 48, 40, 32, 24, 16, 8,
-       57, 49, 41, 33, 25, 17, 9, 1,
+       57, 49, 41, 33, 25, 17,  9, 1,
        59, 51, 43, 35, 27, 19, 11, 3,
        61, 53, 45, 37, 29, 21, 13, 5,
        63, 55, 47, 39, 31, 23, 15, 7]
@@ -16,9 +16,9 @@ ip2 = [40, 8, 48, 16, 56, 24, 64, 32,
        36, 4, 44, 12, 52, 20, 60, 28,
        35, 3, 43, 11, 51, 19, 59, 27,
        34, 2, 42, 10, 50, 18, 58, 26,
-       33, 1, 41, 9, 49, 17, 57, 25]
+       33, 1, 41,  9, 49, 17, 57, 25]
 
-pc1 = [56, 48, 40, 32, 24, 16, 8,
+pc1 = [56, 48, 40, 32, 24, 16,  8,
         0, 57, 49, 41, 33, 25, 17,
         9,  1, 58, 50, 42, 34, 26,
        18, 10,  2, 59, 51, 43, 35,
@@ -27,10 +27,10 @@ pc1 = [56, 48, 40, 32, 24, 16, 8,
        13,  5, 60, 52, 44, 36, 28,
        20, 12,  4, 27, 19, 11, 3]
 
-pc2 = [13, 16, 10, 23, 0, 4,
-       2, 27, 14, 5, 20, 9,
-       22, 18, 11, 3, 25, 7,
-       15, 6, 26, 19, 12, 1,
+pc2 = [13, 16, 10, 23,  0,  4,
+        2, 27, 14,  5, 20,  9,
+       22, 18, 11,  3, 25,  7,
+       15,  6, 26, 19, 12,  1,
        40, 51, 30, 36, 46, 54,
        29, 39, 50, 44, 32, 47,
        43, 48, 38, 55, 33, 52,
@@ -85,10 +85,10 @@ s = [[14, 4, 13, 1, 2, 15, 11, 8, 3, 10, 6, 12, 5, 9, 0, 7,
       7, 11, 4, 1, 9, 12, 14, 2, 0, 6, 10, 13, 15, 3, 5, 8,
       2, 1, 14, 7, 4, 10, 8, 13, 15, 12, 9, 0, 3, 5, 6, 11]]
 
-p = [16, 7, 20, 21, 29, 12, 28, 17,
-     1, 15, 23, 26, 5, 18, 31, 10,
-     2, 8, 24, 14, 32, 27, 3, 9,
-     19, 13, 30, 6, 22, 11, 4, 25]
+p = [16,  7, 20, 21, 29, 12, 28, 17,
+      1, 15, 23, 26,  5, 18, 31, 10,
+      2,  8, 24, 14, 32, 27,  3,  9,
+     19, 13, 30,  6, 22, 11,  4, 25]
 
 rotations = [1, 1, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 1]
 
@@ -106,6 +106,8 @@ def bin_to_word(bword):
 
 
 def bit_string_xor(a, b):
+    assert len(a) == len(b)
+
     return reduce(lambda res, ch: res + ('0' if ch[0] == ch[1] else '1'), zip(a, b), '')
 
 
@@ -122,11 +124,13 @@ def rotate(word, n):
 
 
 def encode_chunk(word, key):
+    assert len(word) == 8
+
     bword = permutate(word_to_bin(word), ip1)
     l0 = bword[0:32]
     r0 = bword[32:64]
 
-    for ki in generate_key(key):
+    for ki in generate_keys(key):
         l1 = r0
         r1 = bit_string_xor(l0, feistel(r0, ki))
         l0 = l1
@@ -139,7 +143,7 @@ def encode_word(word, key):
     return reduce(lambda r, x: r + encode_chunk(word[x:x+8], key), range(0, len(word), 8), '')
 
 
-def generate_key(key):
+def generate_keys(key):
     assert len(key) == 8
 
     key = word_to_bin(key)
@@ -155,6 +159,9 @@ def generate_key(key):
 
 
 def cfb_encrypt(word, iv='01234567', key='76543210'):
+    assert len(iv) == 8, 'IV should have 8 chars'
+    assert len(key) == 8, 'key should have 8 chars'
+
     while not len(word) % 8 == 0:
         word += ' '
     iv = encode_word(iv, key)
@@ -162,7 +169,7 @@ def cfb_encrypt(word, iv='01234567', key='76543210'):
     ct = bit_string_xor(word_to_bin(word[0:8]), iv)
     result = ct
 
-    for i in range(1, len(word), 8):
+    for i in range(8, len(word), 8):
         r = encode_word(ct, key)
         ct = bit_string_xor(word_to_bin(r), word_to_bin(word[i:i+8]))
         result += ct
@@ -171,11 +178,15 @@ def cfb_encrypt(word, iv='01234567', key='76543210'):
 
 
 def cfb_decrypt(cword, iv='01234567', key='76543210'):
+    assert len(cword) % 8 == 0
+    assert len(iv) == 8, 'IV should have 8 chars'
+    assert len(key) == 8, 'key should have 8 chars'
+
     iv = encode_word(iv, key)
-    ct = cword[0:8]
+    ct = cword[0:64]
     plaintext = bin_to_word(bit_string_xor(iv, ct))
 
-    for i in range(1, len(cword), 8):
+    for i in range(64, len(cword), 64):
         r = encode_word(ct, key)
         ct = cword[i:i+8]
         plaintext += bin_to_word(bit_string_xor(ct, r))
@@ -186,7 +197,7 @@ def cfb_decrypt(cword, iv='01234567', key='76543210'):
 def main():
     iv = '01234567'
     key = '87654321'
-    word = 'fsxs'
+    word = 'fsxsdd'
     print(cfb_decrypt(cfb_encrypt(word)))
 
 
